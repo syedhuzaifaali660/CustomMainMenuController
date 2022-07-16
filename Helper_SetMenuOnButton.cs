e5b84436470da262d7e2b3027789b19846290d9f
Syed Huzaifa Ali <80565640+syedhuzaifaali660@users.noreply.github.com>
2022-07-16 12:30:46 +0500

    Add files via upload

---
+++ Helper_SetMenuOnButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Helper_SetMenuOnButton : MonoBehaviour
+{
+    public menuName MenuToOpen;
+    Button btn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn = this.GetComponent<Button>();
+        btn.onClick.AddListener(OpenSelectedMenuOnClick);
+    }
+
+    public void OpenSelectedMenuOnClick()
+    {
+        MenuManager.Instance.OpenMenu(MenuToOpen);
+    }
+}
